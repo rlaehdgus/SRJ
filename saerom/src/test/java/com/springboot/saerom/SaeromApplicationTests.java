890202ddded017028d4626b14267a0e6229d0fcc
김동현 <kimdh@pcninc.co.kr>
2021-05-14 16:47:50 +0900

    초기커밋

---
+++ saerom/src/test/java/com/springboot/saerom/SaeromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.saerom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
